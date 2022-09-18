1e2ee8f4aaa7cd069857c8140fbd3cfc3bed8491
Marco Wegler <marco.wegler@student.uibk.ac.at>
2022-09-18 12:16:50 +0200

    back to home button test

--- Website/pages/maptest/Gruppe4.js
+++ Website/pages/maptest/Gruppe4.js
@@ -8,8 +8,8 @@ const GRUPPE4 = [
         intro: "Gemeinnützige Privatstiftung",
         about: "CONCORDIA ist eine internationale, unabhängige Nichtregierungsorganisation, die es sich zur Aufgabe gemacht hat, Kinder, Jugendliche und Familien in Not auf dem Weg in ein unabhängiges und verantwortungsvolles Leben zu unterstützen. Wir gehen dorthin, wo die Not am größten ist. Und wo andere nicht hingehen. Quelle: https://www.concordia.or.at/de/vision",
         weblink: "https://www.concordia.or.at/",
-        lat:  -15.80896, 
-        lng: -47.94766,
+        lat:  -15.79134, 
+        lng:  -47.88364,
     },
     {
         nr: 2,
--- Website/pages/maptest/map.js
+++ Website/pages/maptest/map.js
@@ -6,6 +6,14 @@ let bounds = [
     [-14, -46] // Northeast coordinates
 ]
 
+
+// magnification with which the map will start
+const zoom = 12;
+// co-ordinates
+const lat = -15.80896;
+const lng = -47.94766;
+
+
 //create map with options 
 const map = L.map('map', {
     fullscreenControl: true,
@@ -35,7 +43,7 @@ let overlays = {
 
 //  add overlays to layers
 let layerControl = L.control.layers({}, {
-    "<img src='icons/city.png' /> Gruppe 1": overlays.Gruppe1,
+    "<img src='icons\bus.png' /> Gruppe 1": overlays.Gruppe1,
     "<img src='icons/health.png' /> Gruppe 2": overlays.Gruppe2,
     "<img src='icons/sozial.png' /> Gruppe3": overlays.Gruppe3,
     "<img src='icons/tree.png' /> Gruppe 4": overlays.Gruppe4
@@ -62,7 +70,7 @@ var LeafIcon = L.Icon.extend({
 });
 
 var Gr1Icon = new LeafIcon({
-        iconUrl: 'icons/tree.png'
+        iconUrl: 'icons/bus.png'
     }),
     Gr2Icon = new LeafIcon({
         iconUrl: 'icons/sozial.png'
@@ -169,3 +177,59 @@ L.control.scale({
 }).addTo(map);
 
 
+const htmlTemplate =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M32 18.451L16 6.031 0 18.451v-5.064L16 .967l16 12.42zM28 18v12h-8v-8h-8v8H4V18l12-9z" /></svg>';
+
+
+// create custom button
+const customControl = L.Control.extend({
+    // button position
+    options: {
+      position: "topleft",
+    },
+  
+    // method
+    onAdd: function (map) {
+      console.log(map.getCenter());
+      // create button
+      const btn = L.DomUtil.create("button");
+      btn.title = "back to home";
+      btn.innerHTML = htmlTemplate;
+      btn.className += "leaflet-bar back-to-home hidden";
+  
+      return btn;
+    },
+  });
+  
+  // adding new button to map controll
+  map.addControl(new customControl());
+  
+  // on drag end
+  map.on("moveend", getCenterOfMap);
+  
+  const buttonBackToHome = document.querySelector(".back-to-home");
+  
+  function getCenterOfMap() {
+    buttonBackToHome.classList.remove("hidden");
+  
+    buttonBackToHome.addEventListener("click", () => {
+      map.flyTo([lat, lng], zoom);
+    });
+  
+    map.on("moveend", () => {
+      const { lat: latCenter, lng: lngCenter } = map.getCenter();
+  
+      const latC = latCenter.toFixed(3) * 1;
+      const lngC = lngCenter.toFixed(3) * 1;
+  
+      const defaultCoordinate = [+lat.toFixed(3), +lng.toFixed(3)];
+  
+      const centerCoordinate = [latC, lngC];
+  
+      if (compareToArrays(centerCoordinate, defaultCoordinate)) {
+        buttonBackToHome.classList.add("hidden");
+      }
+    });
+  }
+  
+  const compareToArrays = (a, b) => JSON.stringify(a) === JSON.stringify(b);
